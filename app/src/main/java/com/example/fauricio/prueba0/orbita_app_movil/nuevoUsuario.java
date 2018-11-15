88b449bb83023295418a47f8990eb780fa9bc362
Fauricio <fauriciocr@gmail.com>
2018-11-14 21:22:50 -0600

    segundo commit

--- app/src/main/java/com/example/fauricio/prueba0/orbita_app_movil/nuevoUsuario.java
+++ app/src/main/java/com/example/fauricio/prueba0/orbita_app_movil/nuevoUsuario.java
@@ -1,106 +1,77 @@
 package com.example.fauricio.prueba0.orbita_app_movil;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.EditText;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
-//import android.support.design.widget.FloatingActionButton;
-
-//import net.sourceforge.jtds.jdbc.*;
-
-public class MainActivity extends AppCompatActivity {
-    private FloatingActionButton nuevo_usuario;
+public class nuevoUsuario extends AppCompatActivity {
     public Connection conexionSql;
-    public static String mensaje = "default";
-    private ListView lista;
-    private ArrayList<Item> ArrayItem = null;
-    private ListAdapter adapter;
+    public EditText nombre,apellido1,apellido2,correo,contraseña;
+    public String nombreAux,apellido1Aux,apellido2Aux,correoAux,contraseñaAux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        lista = findViewById(R.id.ListaUsuario);
-        nuevo_usuario = findViewById(R.id.FB_agregar);
-        nuevo_usuario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), nuevoUsuario.class);
-                startActivity(intent);
-            }
-        });
-        ArrayItem = new ArrayList<>();
-        servicio servicio1 = new servicio();
-        servicio1.execute("");
+        setContentView(R.layout.activity_nuevo_usuario);
+        nombre = findViewById(R.id.et_nombre);
+        apellido1 = findViewById(R.id.et_apellido1);
+        apellido2 = findViewById(R.id.et_apellido2);
+        correo = findViewById(R.id.et_correo);
+        contraseña = findViewById(R.id.et_password);
 
-        //cargarLista(this);
     }
 
-    public void cargarLista(Context context,ResultSet s){
-        try{
-            ArrayItem.add(new Item(R.drawable.userlogo,s.getString("NOMBRE"),s.getString("USUARIO")));
-            while (s.next()) {
-                String nombre = s.getString("NOMBRE");
-                String usuario = s.getString("USUARIO");
-                Log.e("===>>>>>","Nombre: "+nombre+" - Usuario:+ "+usuario);
-                ArrayItem.add(new Item(R.drawable.userlogo,nombre,usuario));
-            }
-            adapter = new ListAdapter(ArrayItem, context);
-            lista.setAdapter(adapter);
-        }catch(SQLException e){
-            Log.e("error >>",e.getMessage());
-        }
-        /*
-        ArrayItem.add(new Item(R.drawable.userlogo,"Livertad","Viva la libertad"));
-        ArrayItem.add(new Item(R.drawable.userlogo,"Desempleo","Desempleado"));
-
-        */
+    public void Registrar(View view){
+        nombreAux = nombre.getText().toString();
+        apellido1Aux = apellido1.getText().toString();
+        apellido2Aux = apellido2.getText().toString();
+        correoAux = correo.getText().toString();
+        contraseñaAux= contraseña.getText().toString();
+        servicio servicioAux = new servicio();
+        servicioAux.execute(nombreAux,apellido1Aux,apellido2Aux,correoAux,contraseñaAux);
     }
 
-    public class servicio extends AsyncTask<String,String,ResultSet>{
+    public class servicio extends AsyncTask<String,String,Boolean> {
 
         @Override
-        protected ResultSet doInBackground(String... params) {
+        protected Boolean doInBackground(String... params) {
             String mensajeAux;
-            ResultSet resultSet = null;
+            Boolean resultSet = null;
             try{
                 conexionSql = connectionclass();
                 if(conexionSql==null){
-                    mensaje = "Revisar acceso a internet";
                     mensajeAux = "Revisar acceso a internet";
                     Log.e("mesaje >>","Referencia nula");
                 }else{
-                    CallableStatement cStmt = conexionSql.prepareCall("{call tablaUsuarios(?)}");
-                    cStmt.setInt(1,1);
-                    resultSet = cStmt.executeQuery();
+                    CallableStatement cStmt = conexionSql.prepareCall("{call INSERTAR_USUARIO(?,?,?,?,?,?,?)}");
+                    cStmt.setString(1,params[0]);
+                    cStmt.setString(2,params[1]);
+                    cStmt.setString(3,params[2]);
+                    cStmt.setString(4,params[3]);
+                    cStmt.setString(5,params[4]);
+                    cStmt.setInt(6,1);
+                    cStmt.setInt(7,1);
+                    resultSet = cStmt.execute();
 
-                    if(resultSet.next()){
-                        mensaje = resultSet.toString();
+                    if(!resultSet){
                         mensajeAux = "Query exitoso";
-
                     }else{
-                        mensaje = "Query no exitoso";
                         mensajeAux = "Query no exitoso";
                     }
                     //CallableStatement callSp = conexionSql.prepareCall("{call TablaProyectos()}");
                 }
             }catch (Exception e){
-                mensaje = "Query invalido";
                 mensajeAux = e.getMessage();
             }
             //textView.setText(mensaje);
@@ -109,9 +80,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(ResultSet s) {
-            cargarLista(getApplicationContext(),s);
-            super.onPostExecute(s);
+        protected void onPostExecute(Boolean aBoolean) {
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            super.onPostExecute(aBoolean);
         }
     }
 
@@ -139,5 +111,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return connection;
     }
-
 }
